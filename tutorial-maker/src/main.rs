7efc0471efc680e47e3818791b0b6d79fbfc103d
Oliver Forral <intrepion@gmail.com>
2022-01-26 02:58:37 -0700

    clones repo

--- tutorial-maker/src/main.rs
+++ tutorial-maker/src/main.rs
@@ -1,3 +1,98 @@
+use std::env;
+use std::process;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let usage = r#"tutorial-maker <username> <program-name> <language> <program-type>
+
+    <language> can only be the following:
+
+    - rust
+
+    <program-type> can only be the following:
+
+    - library
+"#;
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 5 {
+        println!("{usage}");
+
+        process::exit(1);
+    }
+
+    println!("{:?}", args);
+
+    let username = args[1].to_lowercase();
+
+    let program_name = args[2].to_lowercase();
+
+    let args3 = args[3].to_lowercase();
+    let language = if args3 == "rust" {
+        "rust"
+    } else {
+        println!("{usage}");
+
+        process::exit(1);
+    };
+
+    let args4 = args[4].to_lowercase();
+    let program_type = if args4 == "library" {
+        "library"
+    } else {
+        println!("{usage}");
+
+        process::exit(1);
+    };
+
+    let folder_name = format!("{program_name}-{language}-{program_type}");
+
+    let repo_name = format!("{username}/{folder_name}");
+
+    println!("rm -rf {folder_name}");
+
+    let removing_folder_output = Command::new("rm")
+        .arg("-rf")
+        .arg(&folder_name)
+        .output()
+        .expect("removing folder failed");
+
+    if removing_folder_output.status.success() {
+        println!(
+            "cloning stdout: {}",
+            String::from_utf8_lossy(&removing_folder_output.stdout)
+        );
+    } else {
+        println!(
+            "cloning stderr: {}",
+            String::from_utf8_lossy(&removing_folder_output.stderr)
+        );
+
+        process::exit(1);
+    }
+
+    println!("gh repo clone {repo_name}");
+
+    let cloning_output = Command::new("gh")
+        .arg("repo")
+        .arg("clone")
+        .arg(&repo_name)
+        .output()
+        .expect("cloning failed");
+
+    if cloning_output.status.success() {
+        println!(
+            "cloning stdout: {}",
+            String::from_utf8_lossy(&cloning_output.stdout)
+        );
+    } else {
+        println!(
+            "cloning stderr: {}",
+            String::from_utf8_lossy(&cloning_output.stderr)
+        );
+
+        process::exit(1);
+    }
+
+    env::set_current_dir(&folder_name).expect("changing directory failed");    
 }
