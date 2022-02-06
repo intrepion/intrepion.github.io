639657ef937b34f991e4c16484121ea5dfb78bc0
Oliver Forral <intrepion@gmail.com>
2022-02-06 03:00:01 -0700

    red

---
+++ tutorial-maker/src/lib.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod get_repos_should {
+    #[test]
+    fn return_repos_with_language_rust_and_app_name_hello_world_library_and_program_type_lib() {
+        let expected = vec![
+            Repo {},
+        ];
+    }
+}
