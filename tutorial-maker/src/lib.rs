33a436b5a745780b30919d969b3d90ab27474b30
Oliver Forral <intrepion@gmail.com>
2022-02-07 00:48:57 -0700

    blue

--- tutorial-maker/src/lib.rs
+++ tutorial-maker/src/lib.rs
@@ -6,12 +6,12 @@ mod get_repos_should {
     fn return_repos_with_app_name_hello_world_and_language_rust_and_program_type_library_and_user_intrepion(
     ) {
         let expected = vec![GitHubRepository {
-            app_name: "hello-world-library".to_string(),
+            app_name: "hello-world-library".to_owned(),
             client_type: None,
             language: Language::Rust,
             program_type: ProgramType::Library,
             server_type: None,
-            user: "intrepion".to_string(),
+            user: "intrepion".to_owned(),
         }];
 
         let actual = get_repos(
@@ -27,12 +27,12 @@ mod get_repos_should {
     #[test]
     fn return_repos_with_app_name_fizz_buzz_and_language_rust_and_program_type_library_and_user_intrepion() {
         let expected = vec![GitHubRepository {
-            app_name: "fizz-buzz-library".to_string(),
+            app_name: "fizz-buzz-library".to_owned(),
             client_type: None,
             language: Language::Rust,
             program_type: ProgramType::Library,
             server_type: None,
-            user: "intrepion".to_string(),
+            user: "intrepion".to_owned(),
         }];
 
         let actual = get_repos(
@@ -49,12 +49,12 @@ mod get_repos_should {
     fn return_repos_with_app_name_hello_world_and_language_typescript_and_program_type_library_and_user_intrepion(
     ) {
         let expected = vec![GitHubRepository {
-            app_name: "hello-world-library".to_string(),
+            app_name: "hello-world-library".to_owned(),
             client_type: None,
             language: Language::TypeScript,
             program_type: ProgramType::Library,
             server_type: None,
-            user: "intrepion".to_string(),
+            user: "intrepion".to_owned(),
         }];
 
         let actual = get_repos(
@@ -71,12 +71,12 @@ mod get_repos_should {
     fn return_repos_with_app_name_hello_world_and_language_rust_and_program_type_console_and_user_intrepion(
     ) {
         let expected = vec![GitHubRepository {
-            app_name: "hello-world".to_string(),
+            app_name: "hello-world".to_owned(),
             client_type: None,
             language: Language::Rust,
             program_type: ProgramType::Console,
             server_type: None,
-            user: "intrepion".to_string(),
+            user: "intrepion".to_owned(),
         }];
 
         let actual = get_repos(
@@ -116,12 +116,12 @@ fn get_repos(user: &str, app_name: &str, language: Language, program_type: Progr
     vec![GitHubRepository {
         app_name: match program_type {
             ProgramType::Library => format!("{app_name}-library"),
-            _ => app_name.to_string(),
+            _ => app_name.to_owned(),
         },
         client_type: None,
         language,
         program_type,
         server_type: None,
-        user: "intrepion".to_string()
+        user: "intrepion".to_owned()
     }]
 }
