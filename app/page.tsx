92b8afed7f3648682d48dea6eab28f085ede3288
Oliver Forral <intrepion@gmail.com>
2024-03-10 12:16:34 -0600

    Added Tailwind CSS using https://nextjs.org/docs/app/building-your-application/styling/tailwind-css

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
   }
+  
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     output: "export",
     basePath: process.env.NODE_ENV === "production" ? "": undefined,
-    experimental: {
-      appDir: true,
-    },
     images: {
       unoptimized: true,
     },
