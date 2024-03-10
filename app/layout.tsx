b94b298f028eb50a7b9f902107171b217c525353
Oliver Forral <intrepion@gmail.com>
2024-03-10 16:33:28 -0600

    Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from 'next'
- 
+import type { Metadata } from "next";
+
 // These styles apply to every route in the application
-import './globals.css'
- 
+import "./globals.css";
+
 export const metadata: Metadata = {
-  title: 'Oliver Forral',
-  description: 'Oliver Forral\'s Resume',
-}
- 
+  title: "Oliver Forral",
+  description: "Oliver Forral's Resume",
+};
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="bg-gray-100">{children}</body>
     </html>
-  )
+  );
 }
