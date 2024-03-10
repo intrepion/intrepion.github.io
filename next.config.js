4dde6200412e01cabde4ef014258f16c8a19a00e
Oliver Forral <intrepion@gmail.com>
2024-03-09 18:34:27 -0700

    Instructions from https://medium.com/@devcriston/deploy-nextjs-13-app-to-github-pages-714cbf02b140

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
+module.exports = {
+    output: "export",
+    basePath: process.env.NODE_ENV === "production" ? "": undefined,
+    experimental: {
+      appDir: true,
+    },
+    images: {
+      unoptimized: true,
+    },
+    reactStrictMode: true,
   }
-   
-  module.exports = nextConfig
